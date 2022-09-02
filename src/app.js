a93058abb818e09543cc5187c0c8ea8c0e94254e
JingyuMarcelLee <leemar@dickinson.edu>
2022-09-02 01:51:03 -0400

    Tested express. Building DB test needed.

--- src/app.js
+++ src/app.js
@@ -15,3 +15,22 @@ const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log(`Server is up at port:${port}`)
 })
+
+// Route to test database connection
+app.get('/desserts', async (req, res) => {
+    let conn
+    try {
+        conn = await pool.getConnection()
+
+        let sql = `SELECT * FROM desserts`
+        let result = await conn.query(sql)
+
+        res.send(result)
+    } catch (error) {
+        throw error
+    } finally {
+        if (conn) {
+            conn.release()
+        }
+    }
+})
